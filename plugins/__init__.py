628de765e145a873d6eae8017cf73f3d9a7c8009
Troels E. Linnet <tlinnet@gmail.com>
2011-12-03 16:57:26 +0100

    Added plugin_handler

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,18 +1,49 @@
-"""What is __init__.py used for?
-Files named __init__.py are used to mark directories on disk as a Python package directories. If you have the files
+'''
+This script is described at: http://www.pymolwiki.org/index.php/plugindirectory
 
-mydir/spam/__init__.py
-mydir/spam/module.py
-and mydir is on your path, you can import the code in module.py as:
-
-import spam.module
-or
-
-from spam import module
-If you remove the __init__.py file, Python will no longer look for submodules inside that directory, so attempts to import the module will fail.
-
-The __init__.py file is usually empty, but can be used to export selected portions of the package under more convenient names, 
-hold convenience functions, etc. Given the example above, the contents of the __init__ module can be accessed as
-
-import spam
-"""
+PyMOL personal plugin directory
+Recommended location of this file: ~/xxx/Pymol-script-repo/plugins/__init__.py
+'''
+ 
+import os, sys, traceback
+ 
+# import pymolplugins (allow different name)
+pymolplugins = sys.modules[__name__]
+ 
+import pmg_tk.PMGApp
+x__initializePlugins = pmg_tk.PMGApp.initializePlugins
+ 
+def initializePlugins(self):
+    '''
+    Overloaded version of pmg_tk.PMGApp.initializePlugins
+    See pmg_tk/PMGApp.py
+    '''
+    # load global plugins
+    x__initializePlugins(self)
+ 
+    # load user plugins
+    modules = set()
+    for path in pymolplugins.__path__:
+        for filename in os.listdir(path):
+            name, _, ext = filename.partition('.')
+            if ext not in ['py', 'pyc', 'pyo']:
+                if os.path.isdir(os.path.join(path, filename)):
+                    modules.add(filename)
+            elif name != '__init__':
+                modules.add(name)
+    for name in modules:
+        mod_name = pymolplugins.__name__ + '.' + name
+        try:
+            __import__(mod_name, level=0)
+            mod = sys.modules[mod_name]
+            if hasattr(mod,'__init_plugin__'):
+                mod.__init_plugin__(self)
+            elif hasattr(mod,'__init__'):
+                mod.__init__(self)
+        except:
+            print "Exception in plugin '%s' -- Traceback follows..."%name
+            traceback.print_exc()
+            print "Error: unable to initialize plugin '%s'."%name
+ 
+# overload method
+pmg_tk.PMGApp.initializePlugins = initializePlugins
