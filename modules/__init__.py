52e82e99ced4b6076643c01e83019186a4343f78
Troels E. Linnet <tlinnet@gmail.com>
2011-12-04 21:01:44 +0100

    Added function to add to path

---
+++ modules/__init__.py
@@ -0,0 +1,13 @@
+import sys,platform
+sys.path.append('/home/tlinnet/Software/pymol/Pymol-script-repo')
+for path in sys.path:
+    if 'Pymol-script-repo' in path:
+        if platform.system() == 'Windows':
+            pathstring = path + "\\modules\\autodock_tools"
+            #Newdir = os.getcwd()+"\\%s\\"%dirname
+        if platform.system() == 'Linux':
+            pathstring = path + "/modules/autodock_tools"
+            #Newdir = os.getcwd()+"/%s/"%dirname
+        sys.path.append(pathstring)
+        break
+print sys.path
