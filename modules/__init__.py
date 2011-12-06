c3653153f451450d05f9e5338db2f5b64d4dde68
Troels E. Linnet <tlinnet@gmail.com>
2011-12-06 02:55:56 +0100

    uncommented __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,11 +1,11 @@
-import sys,platform
-sys.path.append('/home/tlinnet/Software/pymol/Pymol-script-repo')
-for path in sys.path:
-    if 'Pymol-script-repo' in path:
-        if platform.system() == 'Windows':
-            pathstring = path + "\\modules\\autodock_tools"
-        if platform.system() == 'Linux':
-            pathstring = path + "/modules/autodock_tools"
-        sys.path.append(pathstring)
-        break
-print sys.path
+#import sys,platform
+#sys.path.append('/home/tlinnet/Software/pymol/Pymol-script-repo')
+#for path in sys.path:
+#    if 'Pymol-script-repo' in path:
+#        if platform.system() == 'Windows':
+#            pathstring = path + "\\modules\\autodock_tools"
+#        if platform.system() == 'Linux':
+#            pathstring = path + "/modules/autodock_tools"
+#        sys.path.append(pathstring)
+#        break
+#print sys.path
