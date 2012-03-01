f81f943fb9e50b38982a88058e3303c22c4b99e1
Thomas Holder <speleo3@users.sourceforge.net>
2012-03-01 13:19:34 +0100

    bbPlane code style polishing and minor changes

--- bbPlane.py
+++ bbPlane.py
@@ -1,19 +1,20 @@
+'''
+http://pymolwiki.org/index.php/bbPlane
 
-#
-# -- bbPlane.py - draws a CGO plane across the backbone atoms of
-#                 neighboring amino acids
-# 
-# Author: Jason Vertrees, 06/2010
-#   Modified by Thomas Holder, 06/2010
-#   Modified by Blaine Bell, 08/2011
-# Copyright (C) Schrodinger
-# Open Source License: MIT
-#
-from pymol.cgo import *    # get constants
-from pymol import cmd, stored
-from chempy import cpv
+Draws a CGO plane across the backbone atoms of neighboring amino acids
 
-def bbPlane(objSel='(all)', color='white', transp=0.0, state=1, name=None, quiet=1):
+Author: Jason Vertrees, 06/2010
+  Modified by Thomas Holder, 2010-2012
+  Modified by Blaine Bell, 08/2011
+
+(c) 2010 Schrodinger
+
+License: MIT
+'''
+
+from pymol import cmd
+
+def bbPlane(selection='(all)', color='gray', transp=0.3, state=-1, name=None, quiet=1):
     """
 DESCRIPTION
  
@@ -21,7 +22,7 @@ DESCRIPTION
  
 ARGUMENTS
  
-    objSel = string: protein object or selection {default: (all)}
+    selection = string: protein object or selection {default: (all)}
  
     color = string: color name or number {default: white}
  
@@ -34,6 +35,9 @@ NOTES
     You need to pass in an object or selection with at least two
     amino acids.  The plane spans CA_i, O_i, N-H_(i+1), and CA_(i+1)
     """
+    from pymol.cgo import BEGIN, TRIANGLES, COLOR, VERTEX, END
+    from chempy import cpv
+
     # format input
     transp = float(transp)
     state, quiet = int(state), int(quiet)
@@ -43,22 +47,22 @@ NOTES
     if state < 0:
         state = cmd.get_state()
     elif state == 0:
-        for state in range(1, cmd.count_states(objSel)+1):
-            bbPlane(objSel, color, transp, state, name, quiet)
+        for state in range(1, cmd.count_states(selection)+1):
+            bbPlane(selection, color, transp, state, name, quiet)
         return
 
-    stored.AAs = []
+    AAs = []
     coords = dict()
 
     # need hydrogens on peptide nitrogen
-    cmd.h_add('(%s) and n. N' % objSel)
+    cmd.h_add('(%s) and n. N' % selection)
 
     # get the list of residue ids
-    for obj in cmd.get_object_list(objSel):
-        sel = obj + " and (" + objSel + ")"
+    for obj in cmd.get_object_list(selection):
+        sel = obj + " and (" + selection + ")"
         for a in cmd.get_model(sel + " and n. CA", state).atom:
             key = '/%s/%s/%s/%s' % (obj,a.segi,a.chain,a.resi)
-            stored.AAs.append(key)
+            AAs.append(key)
             coords[key] = [a.coord,None,None]
         for a in cmd.get_model(sel + " and n. O", state).atom:
             key = '/%s/%s/%s/%s' % (obj,a.segi,a.chain,a.resi)
@@ -70,7 +74,7 @@ NOTES
                 coords[key][2] = a.coord
 
     # need at least two amino acids
-    if len(stored.AAs) <= 1:
+    if len(AAs) <= 1:
         print "ERROR: Please provide at least two amino acids, the alpha-carbon on the 2nd is needed."
         return
 
@@ -81,24 +85,24 @@ NOTES
         ]
     obj.extend(cmd.get_color_tuple(color))
 
-    for res in range(0, len(stored.AAs)-1):
-        curIdx, nextIdx = str(stored.AAs[res]), str(stored.AAs[res+1])
+    for res in range(0, len(AAs)-1):
+        curIdx, nextIdx = str(AAs[res]), str(AAs[res+1])
 
         # populate the position array
         pos = [coords[curIdx][0], coords[curIdx][1], coords[nextIdx][2], coords[nextIdx][0]]
 
         # if the data are incomplete for any residues, ignore
         if None in pos:
-            print 'peptide bond %s -> %s incomplete' % (curIdx, nextIdx)
+            if not quiet:
+                print ' bbPlane: peptide bond %s -> %s incomplete' % (curIdx, nextIdx)
             continue
 
         if cpv.distance(pos[0], pos[3]) > 4.0:
-            print '%s and %s not adjacent' % (curIdx, nextIdx)
+            if not quiet:
+                print ' bbPlane: %s and %s not adjacent' % (curIdx, nextIdx)
             continue
 
         # need to order vertices to generate correct triangles for plane
-        #      modified/added by B.Bell 8/18/2011
-        #      modified by Thomas Holder 2012
         if cpv.dot_product(cpv.sub(pos[0], pos[1]), cpv.sub(pos[2], pos[3])) < 0:
             vorder = [0,1,2,2,3,0]
         else:
@@ -113,7 +117,13 @@ NOTES
     obj.append(END)
 
     # update the UI
-    cmd.load_cgo(obj, name, state)
+    cmd.load_cgo(obj, name, state, zoom=0)
     cmd.set("cgo_transparency", transp, name)
 
 cmd.extend("bbPlane", bbPlane)
+
+# tab-completion of arguments
+cmd.auto_arg[0]['bbPlane'] = cmd.auto_arg[1]['color']
+cmd.auto_arg[1]['bbPlane'] = cmd.auto_arg[0]['color']
+
+# vi:expandtab
