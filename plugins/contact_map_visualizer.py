4e108393919da761117cc8432e91bc77ee51ed41
Thomas Holder <speleo3@users.sourceforge.net>
2012-03-21 14:45:52 +0100

    Contact Map Visualizer 2.0

--- plugins/contact_map_visualizer.py
+++ plugins/contact_map_visualizer.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python
+'''
+Contact Map Visualizer 2.0
+
+http://pymolwiki.org/index.php/Contact_Map_Visualizer
+
+Author: Venkatramanan Krishnamani (Version 1.1)
+Author: Thomas Holder (Version 2.0)
+'''
 
 # Copyright Notice
 # ================
@@ -31,165 +38,263 @@
 # PERFORMANCE OF THIS SOFTWARE.
 #
 
-###############################################################################
-#                        Contact Maps Visualizer 1.1                          #
-###############################################################################
+import os
+from pymol import cmd, CmdException
 
-import Tkinter
-import tkFileDialog, tkMessageBox
-import Image
-import pygame as pg
-from pymol import cmd, stored
-import os, sys, urllib2, zlib
-from datetime import datetime
-
-count = 0
-colors = ['red', 'blue', 'green', 'yellow', 'magenta', 'cyan', 'orange', 'marine', 'chartreuse', 'purpleblue', 'violet', 'limon', ]
-colors_value = [(255,0,0), (0,0,255), (0,255,0), (255, 255, 0), (255,0,255), (0, 255, 255), (255, 128, 0), (0, 128,255), (128, 255, 0), (128, 0, 255), (255, 128, 255), (191, 255, 64)]
-dialog = Tkinter.Tk()
-dialog.withdraw()
-height = dialog.winfo_screenheight()
+colors = ['red', 'blue', 'green', 'yellow', 'magenta', 'cyan', 'orange',
+    'marine', 'chartreuse', 'purpleblue', 'violet', 'limon', ]
+colors_value = [tuple(int(i * 255) for i in cmd.get_color_tuple(color))
+    for color in colors]
 
 def __init__(self):
-        self.menuBar.addmenuitem('Plugin', 'command','Contact Map Visualizer',label = 'Contact Map Visualizer',command = lambda s=self : getImageLocation(s))
-    
-def getImageLocation(self):
-        cmd.reinitialize();
-        myFormats = [('Portable Network Graphics','*.png'),('JPEG / JFIF','*.jpg')]
-        try:
-                self.file = tkFileDialog.askopenfile(parent=dialog,mode='rb',filetypes=myFormats, title='Choose the contact map image file')
-        except:
-                quitProgram(self, "No Contact Map!")
-        
-        if self.file != None:
-                data = self.file.read()
-                print "Opening...", self.file.name
-                print "Received %d bytes from this file." % len(data)
-                getPDBLocation(self)
-        else:
-                quitProgram(self, "No Contact Map!")
+    self.menuBar.addmenuitem('Plugin', 'command', 'Contact Map Visualizer',
+            label='Contact Map Visualizer', command = lambda s=self: CMVDialog(s))
 
-def getPDBLocation(self):
-	myFormatsPDB = [('Protein Data Bank','*.pdb'), ('MDL mol','*.mol'), ('PyMol Session File','*.pse')]
-	try:
-		self.pdbFile = tkFileDialog.askopenfile(parent=dialog,mode='rb',filetypes=myFormatsPDB, title='Choose the corresponding PDB file')
-	except:
-		quitProgram(self, "No PDB File!")
-	
-	if self.pdbFile != None:
-		cmd.load(self.pdbFile.name, self.pdbFile.name)
-		print "Opening...", self.pdbFile.name
-		preProcessPDB(self.pdbFile.name)
-		loadImageOnScreen(self.file.name, self.pdbFile.name)
-	else:
-		quitProgram(self,"No PDB file!")
-		
-	
-def quitProgram(self,tit):
-        tkMessageBox.showinfo(tit, "Quitting now...!")
-        cmd.reinitialize();
+def CMVDialog(self):
+    import tkFileDialog, tkMessageBox
 
-def preProcessPDB(fname):
-        stored.residues = []
-        stored.chains = []
-        cmd.iterate('(name ca)', 'stored.residues.append(resi)')
-        cmd.iterate('(name ca)', 'stored.chains.append(chain)')
-        cmd.show_as("cartoon", '(all)')
-        cmd.color("white", '(all)')
-        
-def loadImageOnScreen(image_file, pdb_file):
-        
-        #General variables
-        global count
-        sel = 0
-        ntime  = datetime.now()
-        gray = (100,100,100)
-        BLACK = (0, 0, 0)
-        WHITE = (255, 255, 255)
-        outputname = "%s_selectedPoints_%d-%d-%d_%d%d.png"%(os.path.basename(image_file),ntime.day, ntime.month, ntime.year, ntime.hour, ntime.minute)
-        pdb_outputname = "%s_selectedPoints_%d-%d-%d_%d%d.pse"%(os.path.basename(pdb_file),ntime.day, ntime.month, ntime.year, ntime.hour, ntime.minute)
-        
-        #Text related arrays nd variables
-        text = []
-        textRect = []
-        textcount = -1
+    try:
+        import pygame as pg
+    except ImportError:
+        tkMessageBox.showerror('Error', 'This plugin requires the "pygame" module')
+        return
 
-        # use an image you have (.bmp  .jpg  .png  .gif)
-        im = Image.open(image_file)
-        sw = height - 100
-        sh = height - 100
-        factor = float(float(im.size[0])/float(sw))
-        
-        # initialize pygame
-        pg.init()
-        screen = pg.display.set_mode((sw, sh))
-        pg.display.set_caption('Choose the location by clicking')
-        image = pg.image.load(image_file).convert()
-        image = pg.transform.scale(image, (sw,sh))
-        image_rect = image.get_rect()
-        
-        running = True
-        while running:
-                event = pg.event.poll()
-                keyinput = pg.key.get_pressed()
-                
-                # exit on corner 'x' click or escape key press
-                if keyinput[pg.K_ESCAPE]:
-                        running = False
-                        pg.quit()
-                        cmd.reinitialize();
-                        break
-                elif event.type == pg.QUIT:
-                        running = False
-                        pg.quit()
-                        cmd.reinitialize();
-                        break
-                elif event.type == pg.MOUSEBUTTONDOWN:
-                        sel = 1
-                        textcount += 1
-                        coor = [event.pos[0]*factor,(sh - event.pos[1])*factor]
-                        updateSelections(coor)
-                        if count >= 10:
-                                count = 0
-                        pg.draw.circle(image, colors_value[count], event.pos, 3, 0)
-                        
-                        # set up fonts
-                        basicFont = pg.font.SysFont("Arial", 12)
-                        name = "(%s%s, %s%s)"%(stored.residues[int(coor[0])], stored.chains[int(coor[0])],stored.residues[int(coor[1])], stored.chains[int(coor[1])])
-                        # set up the text
-                        text.append(basicFont.render(name, True, WHITE, BLACK))
-                        textRect.append(text[textcount].get_rect())
-                        textRect[textcount][0] = event.pos[0] + 5
-                        textRect[textcount][1] = event.pos[1] + 5
-                        
-                        screen.blit(image, image_rect)
-                        for a in range(textcount+1):
-                                screen.blit(text[a], textRect[a])
-                        pg.display.flip()
-                        pg.image.save(screen, outputname)
-                        cmd.save(pdb_outputname,'(all)',-1,"pse")
-                        count += 1
-                # update screen
-                screen.blit(image, image_rect)
-                if sel == 1:
-                        for a in range(textcount+1):
-                                screen.blit(text[a], textRect[a])
-                pg.display.flip()
+    myFormats = [('Portable Network Graphics','*.png'),('JPEG / JFIF','*.jpg')]
+    try:
+        image_file = tkFileDialog.askopenfilename(parent=self.root,
+                filetypes=myFormats, title='Choose the contact map image file')
+        if not image_file:
+            raise
+    except:
+        tkMessageBox.showerror('Error', 'No Contact Map!')
+        return
 
-def updateSelections(residues):
-        global count
-        print "Residue Numbers of Current Selection (screenshot saved)"
-        print "x", stored.residues[int(residues[0])], stored.chains[int(residues[0])]
-        print "y", stored.residues[int(residues[1])], stored.chains[int(residues[1])]
-        selectionname = "%s%s_%s%s"%(stored.residues[int(residues[0])], stored.chains[int(residues[0])], stored.residues[int(residues[1])], stored.chains[int(residues[1])])
-        
-        cmd.select(selectionname, "resid %s and chain %s + resid %s and chain %s"%(stored.residues[int(residues[0])],stored.chains[int(residues[0])],stored.residues[int(residues[1])], stored.chains[int(residues[1])]))
-        
-        cmd.set('stick_radius', '0.2')
-        cmd.show("sticks", selectionname)
-        cmd.set('sphere_scale', '0.25')
-        cmd.show("spheres", selectionname)
+    myFormatsPDB = [('Protein Data Bank','*.pdb'), ('MDL mol','*.mol'), ('PyMol Session File','*.pse')]
+    try:
+        pdb_file = tkFileDialog.askopenfilename(parent=self.root,
+                filetypes=myFormatsPDB, title='Choose the corresponding PDB file')
+        if not pdb_file:
+            raise
+    except:
+        tkMessageBox.showerror('Error', 'No PDB file!')
+        return
 
-        if count >= 10:
+    name = cmd.get_unused_name('protein')
+    cmd.load(pdb_file, name)
+    contact_map_visualizer(image_file, name, 1, 0)
+
+def contact_map_visualizer(image_file='', selection='all', screenshots=0, quiet=1):
+    '''
+DESCRIPTION
+
+    Contact map visualizer
+
+    If no image_file is provided, try to run "contact_map_generator" first.
+
+USAGE
+
+    contact_map_visualizer [ image_file [, selection [, screenshots ]]]
+
+SEE ALSO
+
+    contact_map_generator
+    '''
+    if not image_file:
+        import tempfile
+        image_file = tempfile.mktemp(suffix='.png')
+        if not quiet:
+            print ' Writing image to', image_file
+        contact_map_generator(image_file, selection, quiet=quiet)
+
+    import threading
+    t = threading.Thread(target=_contact_map_visualizer, kwargs=locals())
+    t.setDaemon(1)
+    t.start()
+
+def _contact_map_visualizer(image_file, selection, screenshots, quiet, **kwargs):
+    from datetime import datetime
+
+    try:
+        import pygame as pg
+    except ImportError:
+        print ' Error: This plugin requires the "pygame" module'
+        raise CmdException
+
+    screenshots, quiet = int(screenshots), int(quiet)
+
+    cmd.set('sphere_scale', 0.25)
+
+    idx_list = []
+    cmd.iterate('(%s) and name CA' % (selection),
+            'idx_list.append(((model,index),chain,resi))', space=locals())
+    cmd.color("white", selection)
+
+    # General variables
+    count = 0
+    sel = 0
+    ntime  = datetime.now()
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+
+    if screenshots:
+        outputname = "%s_selectedPoints_%d-%d-%d_%d%d.png" % (os.path.basename(image_file),
+                ntime.day, ntime.month, ntime.year, ntime.hour, ntime.minute)
+
+    # Text related arrays nd variables
+    text = []
+    textRect = []
+    textcount = -1
+
+    # screen height
+    try:
+        screen_height = cmd.pymol._ext_gui.root.winfo_screenheight()
+    except:
+        print ' Warning: could not determine screen height'
+        screen_height = 700
+
+    # use an image you have (.bmp  .jpg  .png  .gif)
+    image = pg.image.load(image_file)
+    image_height = image.get_height()
+
+    factor = max((screen_height - 100) / image_height, 1)
+    height = image_height * factor
+    factor = 1.0 / factor
+
+    if image_height != len(idx_list):
+        print ' Warning: Dimension of image and number of CA atoms in selection',
+        print 'differ! (%d vs. %d)' % (image_height, len(idx_list))
+
+    # initialize pygame
+    pg.init()
+    screen = pg.display.set_mode((height, height))
+    pg.display.set_caption('Choose the location by clicking')
+    image = pg.transform.scale(image.convert(), (height, height))
+    image_rect = image.get_rect()
+
+    while True:
+        event = pg.event.poll()
+        keyinput = pg.key.get_pressed()
+
+        # exit on corner 'x' click or escape key press
+        if keyinput[pg.K_ESCAPE] or event.type == pg.QUIT:
+            break
+
+        if event.type == pg.MOUSEBUTTONDOWN:
+            coor = [event.pos[0] * factor, (height - event.pos[1]) * factor]
+
+            try:
+                idx1, chain1, resi1 = idx_list[int(coor[0])]
+                idx2, chain2, resi2 = idx_list[int(coor[1])]
+            except IndexError:
+                print ' Error: selection to small'
+                continue
+
+            print ' You clicked %s/%s/ %s/%s/' % (chain1, resi1, chain2, resi2)
+
+            sel = 1
+            textcount += 1
+
+            selectionname = '%s%s_%s%s' % (resi1, chain1, resi2, chain2)
+            cmd.select(selectionname, 'byres (%s`%d %s`%d)' % tuple(idx1 + idx2))
+
+            cmd.show('sticks', selectionname)
+            cmd.show('spheres', selectionname)
+
+            cmd.color(colors[count], selectionname)
+            cmd.center(selectionname, animate=1)
+
+            pg.draw.circle(image, colors_value[count], event.pos, 3, 0)
+
+            # set up fonts
+            basicFont = pg.font.SysFont('Arial', 12)
+            name = '(%s%s, %s%s)' % (resi1, chain1, resi2, chain2)
+
+            # set up the text
+            text.append(basicFont.render(name, True, WHITE, BLACK))
+            textRect.append(text[textcount].get_rect())
+            textRect[textcount][0] = event.pos[0] + 5
+            textRect[textcount][1] = event.pos[1] + 5
+            
+            screen.blit(image, image_rect)
+            for a in range(textcount+1):
+                screen.blit(text[a], textRect[a])
+            pg.display.flip()
+
+            if screenshots:
+                if not quiet:
+                    print ' Writing image to', outputname
+                pg.image.save(screen, outputname)
+
+            count += 1
+            if count > len(colors):
                 count = 0
-        cmd.color(colors[count], selectionname)
+
+        # update screen
+        screen.blit(image, image_rect)
+        if sel == 1:
+            for a in range(textcount+1):
+                screen.blit(text[a], textRect[a])
+        pg.display.flip()
+
+    pg.quit()
+
+def contact_map_generator(filename, selection='all', state=-1, quiet=1):
+    '''
+DESCRIPTION
+
+    Genarate a contact map image with "g_mdmat" (requires gromacs)
+
+USAGE
+
+    contact_map_generator filename [, selection [, state ]]
+    '''
+    import os, shutil, tempfile, subprocess
+    import re, Image
+    from StringIO import StringIO
+
+    state, quiet = int(state), int(quiet)
+
+    if not os.path.splitext(filename)[-1].lower() in ['.png', '.jpg', '.jpeg']:
+        print ' Error: filename must have png or jpg extension'
+        raise CmdException
+
+    try:
+        tempdir = tempfile.mkdtemp()
+        file_f = os.path.join(tempdir, 'f.pdb')
+        file_mean = os.path.join(tempdir, 'dm.xpm')
+
+        cmd.save(file_f, selection, state)
+
+        if state == 0:
+            file_s = os.path.join(tempdir, 's.pdb')
+            cmd.save(file_s, selection)
+        else:
+            file_s = file_f
+
+        process = subprocess.Popen(['g_mdmat', '-f', file_f, '-s', file_s, '-mean', file_mean],
+                stdin=subprocess.PIPE)
+        print >> process.stdin, 'Protein-H'
+        process.stdin.close()
+        process.wait()
+
+        # get XPM file contents
+        xpm = open(file_mean).read()
+
+    except OSError:
+        print ' Error: calling external applications failed'
+        raise CmdException
+    finally:
+        shutil.rmtree(tempdir)
+
+    # fix XPM and convert to PNG or JPG
+    xpm = re.sub(r'/\*.*?\*/', '', xpm) # strip comments
+    xpm = re.sub(r'  +', ' ', xpm)      # strip multi-spaces
+    xpm = re.sub(r'\n\s+', '\n', xpm)   # strip empty lines
+    xpm = '/* XPM */' + xpm
+    image = Image.open(StringIO(xpm))
+    image.save(filename)
+
+cmd.extend('contact_map_visualizer', contact_map_visualizer)
+cmd.extend('contact_map_generator', contact_map_generator)
+
+# vi:expandtab:smarttab:sw=4
