581339cec1c209f5ad48491f715d41b2ef56633d
Mateusz Maciejewski <matt@mattmaciejewski.com>
2013-04-29 18:50:50 +0300

    Update inertia_tensor.py

--- inertia_tensor.py
+++ inertia_tensor.py
@@ -1,5 +1,5 @@
 '''
-http://www.pymolwiki.org/index.php/inertia_tensor.py
+http://www.pymolwiki.org/index.php/inertia_tensor
  
 (c) August 2010 by Mateusz Maciejewski
 matt (at) mattmaciejewski . com
@@ -12,12 +12,12 @@ from pymol.cgo import *
 from pymol import cmd
 
 
-def tensor(selection, name="tensor", state=1, quiet=1):
+def tensor(selection, name="tensor", state=1, scaling=0, quiet=1):
 
     """
 DESCRIPTION
     
-    This script will draw the eigenvectors of the inertia tensor of the selection.
+    This script will draw the inertia tensor of the selection.
 
 ARGUMENTS
     
@@ -27,10 +27,14 @@ ARGUMENTS
     
     state = int: state/model in the molecule object used in the tensor calculation
 
+    scaling = int {0, 1, or 2}: 0 for no scaling of the inertia axes, 1 for scaling
+    according to the molecular shape, 2 for scaling according to the eigenvalues 
+    {default: 0}
+
 EXAMPLE
     
     PyMOL> run inertia_tensor.py
-    PyMOL> tensor molecule_object & i. 2-58+63-120 & n. n+ca+c, "tensor_model5_dom2", 5
+    PyMOL> tensor molecule_object & i. 2-58+63-120 & n. n+ca+c, "tensor_model5_dom2", 5, 1
     
 NOTES
     
@@ -41,28 +45,31 @@ NOTES
     import numpy
 
 
-    def draw_axes(start,ends,radius=.2,name_obj="tensor"):
+    def draw_axes(start,ends,cone_ends,radius=.2,name_obj="tensor"):
         radius = float(radius)
         size = radius*15.
-        origin_offset = radius * -25.
-        obj = [
-            CYLINDER, start[0],start[1],start[2],ends[0][0]+start[0],ends[0][1]+start[1],ends[0][2]+start[2], radius, 1.0, 1.0, 1.0, 1.0, 0.0, 0.,  # red Ixx
-            CYLINDER, start[0],start[1],start[2],(-1)*ends[0][0]+start[0],(-1)*ends[0][1]+start[1],(-1)*ends[0][2]+start[2], radius, 1.0, 1.0, 1.0, 1.0, 0.0, 0.,
-            CYLINDER, start[0],start[1],start[2],ends[1][0]+start[0],ends[1][1]+start[1],ends[1][2]+start[2], radius, 1.0, 1.0, 1.0, 0., 1.0, 0.,   # green Iyy
-            CYLINDER, start[0],start[1],start[2],(-1)*ends[1][0]+start[0],(-1)*ends[1][1]+start[1],(-1)*ends[1][2]+start[2], radius, 1.0, 1.0, 1.0, 0., 1.0, 0.,
-            CYLINDER, start[0],start[1],start[2],ends[2][0]+start[0],ends[2][1]+start[1],ends[2][2]+start[2], radius, 1.0, 1.0, 1.0, 0., 0.0, 1.0,  # blue Izz
-            CYLINDER, start[0],start[1],start[2],(-1)*ends[2][0]+start[0],(-1)*ends[2][1]+start[1],(-1)*ends[2][2]+start[2], radius, 1.0, 1.0, 1.0, 0., 0.0, 1.0,
 
+        obj = [
+            CYLINDER, start[0], start[1], start[2], ends[0][0] + start[0], ends[0][1] + start[1], ends[0][2] + start[2], radius, 1.0, 1.0, 1.0, 1.0, 0.0, 0.,
+            CYLINDER, start[0], start[1], start[2], (-1) * ends[0][0] + start[0], (-1) * ends[0][1] + start[1], (-1) * ends[0][2] + start[2], radius, 1.0, 1.0, 1.0, 1.0, 0.0, 0.,
+            CYLINDER, start[0], start[1], start[2], ends[1][0] + start[0], ends[1][1] + start[1], ends[1][2] + start[2], radius, 1.0, 1.0, 1.0, 0., 1.0, 0.,
+            CYLINDER, start[0], start[1], start[2], (-1) * ends[1][0] + start[0], (-1) * ends[1][1] + start[1], (-1) * ends[1][2] + start[2], radius, 1.0, 1.0, 1.0, 0., 1.0, 0.,
+            CYLINDER, start[0], start[1], start[2], ends[2][0] + start[0], ends[2][1] + start[1], ends[2][2] + start[2], radius, 1.0, 1.0, 1.0, 0., 0.0, 1.0,
+            CYLINDER, start[0], start[1], start[2], (-1) * ends[2][0] + start[0], (-1) * ends[2][1] + start[1], (-1) * ends[2][2] + start[2], radius, 1.0, 1.0, 1.0, 0., 0.0, 1.0,
+            CONE, ends[0][0] + start[0], ends[0][1] + start[1], ends[0][2] + start[2], cone_ends[0][0] + start[0], cone_ends[0][1] + start[1], cone_ends[0][2] + start[2], 0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0,
+            CONE, (-1) * ends[0][0] + start[0], (-1) * ends[0][1] + start[1], (-1) * ends[0][2] + start[2], (-1) * cone_ends[0][0] + start[0], (-1) * cone_ends[0][1] + start[1], (-1) * cone_ends[0][2] + start[2], 0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0,
+            CONE, ends[1][0] + start[0], ends[1][1] + start[1], ends[1][2] + start[2], cone_ends[1][0] + start[0], cone_ends[1][1] + start[1], cone_ends[1][2] + start[2], 0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,
+            CONE, (-1) * ends[1][0] + start[0], (-1) * ends[1][1] + start[1], (-1) * ends[1][2] + start[2], (-1) * cone_ends[1][0] + start[0], (-1) * cone_ends[1][1] + start[1], (-1) * cone_ends[1][2] + start[2], 0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,
+            CONE, ends[2][0] + start[0], ends[2][1] + start[1], ends[2][2] + start[2], cone_ends[2][0] + start[0], cone_ends[2][1] + start[1], cone_ends[2][2] + start[2], 0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0,
+            CONE, (-1) * ends[2][0] + start[0], (-1) * ends[2][1] + start[1], (-1) * ends[2][2] + start[2], (-1) * cone_ends[2][0] + start[0], (-1) * cone_ends[2][1] + start[1], (-1) * cone_ends[2][2] + start[2], 0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0,
             ]
 
         cmd.load_cgo(obj,name_obj)
 
 
-
     totmass=0.0
     x_com,y_com,z_com=0,0,0
-    I11,I12,I13,I21,I22,I23,I31,I32,I33=0,0,0,0,0,0,0,0,0
-
+    
     model=cmd.get_model(selection, state)
 
     for a in model.atom:
@@ -102,7 +109,6 @@ NOTES
    
 
     tensor = numpy.array([(I[0:3]),(I[3:6]),(I[6:9])])
-    eigens = numpy.linalg.eig(tensor)
     vals,vects = numpy.linalg.eig(tensor) # they come out unsorted, so the command below is needed
 
     eig_ord = numpy.argsort(vals) # a thing to note is that here COLUMN i corrensponds to eigenvalue i.
@@ -112,21 +118,40 @@ NOTES
 
     if not int(quiet):
         print
-        print "Inertia tensor x, y, z eigenvalues:"
+        print "Inertia tensor z, y, x eigenvalues:"
         print
         print ord_vals
         print
-        print "Inertia tensor x, y, z eigenvectors:"
+        print "Inertia tensor z, y, x eigenvectors:"
         print
         print ord_vects
 
 
-    start=[x_com,y_com,z_com]
-    ends=[[10*ord_vects[0][0],10*ord_vects[0][1],10*ord_vects[0][2]],
-          [10*ord_vects[1][0],10*ord_vects[1][1],10*ord_vects[1][2]],
-          [10*ord_vects[2][0],10*ord_vects[2][1],10*ord_vects[2][2]]]
+    if int(scaling) == 0:
+        norm_vals = [sum(numpy.sqrt(ord_vals/totmass)) / 3 for i in range(3)]
 
-    draw_axes(start,ends,name_obj=name)
+    elif int(scaling) == 1:
+        normalizer = numpy.sqrt(max(ord_vals)/totmass)
+        norm_vals = normalizer / numpy.sqrt(ord_vals/totmass) * normalizer
+        norm_vals = norm_vals / (max(norm_vals) / min(norm_vals))
+
+    elif int(scaling) == 2:
+        normalizer = numpy.sqrt(max(ord_vals)/totmass)
+        norm_vals = numpy.sqrt(ord_vals/totmass)
+        
+
+    start=[x_com,y_com,z_com]
+    ends=[[(norm_vals[0] - 1)*ord_vects[0][0],(norm_vals[0] - 1)*ord_vects[0][1],(norm_vals[0] - 1)*ord_vects[0][2]],
+          [(norm_vals[1] - 1)*ord_vects[1][0],(norm_vals[1] - 1)*ord_vects[1][1],(norm_vals[1] - 1)*ord_vects[1][2]],
+          [(norm_vals[2] - 1)*ord_vects[2][0],(norm_vals[2] - 1)*ord_vects[2][1],(norm_vals[2] - 1)*ord_vects[2][2]]]
+    
+    cone_ends=[[norm_vals[0]*ord_vects[0][0],norm_vals[0]*ord_vects[0][1],norm_vals[0]*ord_vects[0][2]],
+               [norm_vals[1]*ord_vects[1][0],norm_vals[1]*ord_vects[1][1],norm_vals[1]*ord_vects[1][2]],
+               [norm_vals[2]*ord_vects[2][0],norm_vals[2]*ord_vects[2][1],norm_vals[2]*ord_vects[2][2]]]
+
+    draw_axes(start,ends,cone_ends,name_obj=name)
 
 
 cmd.extend("tensor", tensor)
+  
+
