d5a2cc502b90578c38597b0670d2566fdf27f152
Justin Lecher <jlec@gentoo.org>
2013-07-26 17:12:15 +0200

    Lots of improvements for I/O of cns tbl file and grouping of restraints.
    Signed-off-by: Justin Lecher <jlec@gentoo.org>

--- plot_noe.py
+++ plot_noe.py
@@ -8,8 +8,9 @@ License: BSD-2-Clause
 '''
 
 from pymol import cmd, CmdException
+import re
 
-def plot_noe(filename, selection='', line_color='gray20', line_width='1.0', single=0, quiet=1):
+def plot_noe(filename, selection='', line_color='gray20', line_width='1.0', single=0, quiet=1, aria=0, per_atom=0, per_residue=1):
     """
 DESCRIPTION
 
@@ -25,6 +26,12 @@ ARGUMENTS
 
     line_width = float: The thickness of the NOE lines. {default: 1.0}
 
+    aria = integer: Name NOEs after Aria IDs.
+
+    per_atom: Group NOEs on atom basis
+
+    per_residue: Group NOEs on residue basis (default)
+
 NOE Restraint Format Example
 
     assign (residue 5 and name HB#) (residue 21 and name HA) 3.0 0.7 0.7
@@ -35,39 +42,78 @@ EXAMPLE
     """
     from pymol.parsing import split
 
-    single, quiet = int(single), int(quiet)
+    single, quiet, aria, per_residue, per_atom = int(single), int(quiet), int(aria), int(per_residue), int(per_atom)
 
     count = 0
+    parent_id = None
+
+    rgx_restraint = re.compile(
+                        r"""
+                            \s*(?P<parent>\w*)\s+\(
+                            (segid\s+\"\s*(?P<src_segid>[\w\d]+)\"\s+and\s+)?
+                            resid\s+(?P<src_resid>\d+)\s+and\s+
+                            name\s+(?P<src_atom>[\w\d]+)
+                            \)\s+\(
+                            (segid\s+\"\s+(?P<dst_segid>[\w\d]+)\"\s+and\s+)?
+                            resid\s*(?P<dst_resid>\d+)\s+and\s+
+                            name\s*(?P<dst_atom>[\w\d]+)
+                            \)
+                            (\s+(?P<dist>[\d\.]+))?
+                            (.*id=(?P<ID>\d+))?
+                            .*
+                        """, re.X)
+
 
     for line in open(filename):
-        line = line.replace('(', ' (').replace(')', ') ').replace('#', '*')
-
-        a = filter(None, split(line, ' \t'))
-        if len(a) < 4 or not a[0].startswith('assi'):
-            continue
-
-        try:
-            float(a[3])
-        except ValueError:
-            continue
-
-        sele1, sele2 = a[1:3]
-
-        if selection:
-            sele1 = '(%s) and (%s)' % (selection, sele1)
-            sele2 = '(%s) and (%s)' % (selection, sele2)
-
-        label = single and "NOE" or ("NOE_%d" % (count + 1))
-
-        try:
-            cmd.distance(label, sele1, sele2, quiet=quiet,
-                    width=line_width, gap=0, label=0)
-        except CmdException:
-            print 'FAILED: %s - %s' % (sele1, sele2)
-            continue
-
-        cmd.set("dash_color", line_color, label)
+        restraint = rgx_restraint.match(line)
         count += 1
+
+        if restraint:
+            restraint = restraint.groupdict()
+
+            if restraint["src_segid"] == None:
+                restraint["src_segid"] = "A"
+
+            if restraint["dst_segid"] == None:
+                restraint["dst_segid"] = "A"
+
+            if restraint["parent"][:4].upper() == "ASSI":
+                parent = restraint
+
+            sele1 = 'segid %s and resi %s and name %s' % (restraint["src_segid"], restraint["src_resid"], restraint["src_atom"])
+            sele2 = 'segid %s and resi %s and name %s' % (restraint["dst_segid"], restraint["dst_resid"], restraint["dst_atom"])
+
+            if single:
+                label = "NOE"
+
+            elif aria and restraint["ID"]:
+                label = "NOE_" + restraint["ID"]
+            elif aria and parent and parent["ID"]:
+                label = "NOE_" + parent["ID"]
+
+            elif per_atom:
+                label = "NOE_%s_%s_%s" % (restraint["dst_segid"], restraint["dst_resid"], restraint["dst_atom"])
+            elif per_residue:
+                label = "NOE_%s_%s" % (restraint["dst_segid"], restraint["dst_resid"])
+
+            elif restraint["parent"][:4].upper() == "ASSI":
+                label = parent_id
+            else:
+                parent_id = "NOE_%d" % count
+                label = parent_id
+
+            try:
+                cmd.distance(label, sele1, sele2, quiet=quiet,
+                        width=line_width, gap=0, label=0)
+            except CmdException:
+                print 'FAILED: %s - %s' % (sele1, sele2)
+                continue
+
+            cmd.set("dash_color", line_color, label)
+        else:
+            print line
+
+    cmd.order("NOE*", "yes")
 
     if not quiet:
         print ' Info: Created distance objects for %d restraints' % (count)
