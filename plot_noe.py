ed166f233cf3a9b9105a51363b0e4b0c1b766008
Justin Lecher <jlec@gentoo.org>
2015-02-06 14:32:29 +0100

    Enhance noe plotting script
    * pep8 conform
    * allow more generic input
    * add advanced coloring option for distance dependent coloring
    * ...
    
    Signed-off-by: Justin Lecher <jlec@gentoo.org>

--- plot_noe.py
+++ plot_noe.py
@@ -1,17 +1,19 @@
-'''
+"""
 http://pymolwiki.org/index.php/plot_noe
 
 (c) 2011-09-23 by Justin L Lorieau
 (c) 2012-10-31 by Thomas Holder
 
 License: BSD-2-Clause
-'''
+"""
 
 from pymol import cmd, CmdException
 import re
+import shlex
 
 
-def plot_noe(filename, selection='', line_color='gray20', line_width='1.0', single=0, quiet=1, aria=0, per_atom=0, per_residue=1):
+def plot_noe(filename, line_color=None, advanced_coloring=0, line_width='1.0', single=0, quiet=1, aria=0, per_atom=0,
+             per_residue=1):
     """
 DESCRIPTION
 
@@ -20,6 +22,8 @@ DESCRIPTION
 ARGUMENTS
 
     filename = string: The filename of the NOE retraint file in XPLOR NIH format.
+
+    single = string: create a single object for all restraints.
 
     selection = string: atom selection {default: all}
 
@@ -41,81 +45,179 @@ EXAMPLE
 
     PyMOL> plot_noe noe_short.tbl
     """
-    from pymol.parsing import split
 
-    single, quiet, aria, per_residue, per_atom = int(single), int(quiet), int(aria), int(per_residue), int(per_atom)
+    single, quiet, aria, per_residue, per_atom, advanced_coloring = \
+        int(single), int(quiet), int(aria), int(per_residue), int(per_atom), int(advanced_coloring)
 
     count = 0
-    parent_id = None
 
-    rgx_restraint = re.compile(
-        r"""
-                            \s*(?P<parent>\w*)\s+\(
-                            (segid\s+\"\s*(?P<src_segid>[\w\d]+)\"\s+and\s+)?
-                            resid\s+(?P<src_resid>\d+)\s+and\s+
-                            name\s+(?P<src_atom>[\w\d]+)
-                            \)\s+\(
-                            (segid\s+\"\s+(?P<dst_segid>[\w\d]+)\"\s+and\s+)?
-                            resid\s*(?P<dst_resid>\d+)\s+and\s+
-                            name\s*(?P<dst_atom>[\w\d]+)
-                            \)
-                            (\s+(?P<dist>[\d\.]+))?
-                            (.*id=(?P<ID>\d+))?
-                            .*
-                        """, re.X)
+    if advanced_coloring == 1:
+        cmd.set("group_auto_mode", 2)
+
+    rgx_assi = re.compile("\s*[asignASIGN]+\s+.*")
+    rgx_or = re.compile("\s*[orOR]+\s+.*")
+    rgx_bracket = re.compile("(\(|\))")
+    rgx_tick = re.compile("(\w)\'")
+
+    restraint_line = None
+    restraint_block = []
+    restraints_blocks = []
 
     for line in open(filename):
-        restraint = rgx_restraint.match(line)
-        count += 1
+        if rgx_assi.match(line):
+            if restraint_line:
+                restraint_block.append(rgx_tick.sub("\g<1>^", rgx_bracket.sub(" ", restraint_line)))
+                restraints_blocks.append(restraint_block)
+            restraint_block = []
+            restraint_line = line
+        elif rgx_or.match(line):
+            restraint_block.append(rgx_tick.sub("\g<1>^", rgx_bracket.sub(" ", restraint_line)))
+            restraint_line = line
+        else:
+            restraint_line += line
 
-        if restraint:
-            restraint = restraint.groupdict()
+    restraint_block.append(rgx_tick.sub("\g<1>^", rgx_bracket.sub(" ", restraint_line)))
+    restraints_blocks.append(restraint_block)
 
-            if restraint["src_segid"] == None:
-                restraint["src_segid"] = "A"
+    for restraint_block in restraints_blocks:
+        restraints = {"connections": []}
 
-            if restraint["dst_segid"] == None:
-                restraint["dst_segid"] = "A"
+        try:
+            shlex_split = shlex.split(restraint_block[0])
+        except ValueError:
+            print 'Cannot process "%s"' % restraint_block[0]
+        try:
+            restraints["distance"] = float(shlex_split[11].strip())
+        except IndexError:
+            print 'Cannot process "%s"' % "".join(restraint_block)
+            continue
+        except ValueError:
+            try:
+                restraints["distance"] = float(shlex_split[17].strip())
+            except ValueError:
+                print "Failed to extract distance, setting to 1A"
+                restraints["distance"] = 1
 
-            if restraint["parent"][:4].upper() == "ASSI":
-                parent = restraint
+        if len(shlex_split) > 20:
+            restraints["ID"] = shlex_split[26][3:-1].strip()
+        else:
+            restraints["ID"] = str(count + 1)
 
-            sele1 = 'segid %s and resi %s and name %s' % (restraint["src_segid"], restraint["src_resid"], restraint["src_atom"])
-            sele2 = 'segid %s and resi %s and name %s' % (restraint["dst_segid"], restraint["dst_resid"], restraint["dst_atom"])
+        for restraint_line in restraint_block:
+            distance = {}
+            try:
+                shlex_split = shlex.split(restraint_line)
 
+                if len(shlex_split) > 6:
+                    if shlex_split[1].strip().lower() == shlex_split[9].strip().lower() == "segid":
+                        distance["src_segid"] = shlex_split[2].strip()
+                        distance["src_resid"] = shlex_split[5].strip()
+                        distance["src_atom"] = shlex_split[8].strip().replace("^", "'")
+                        distance["dst_segid"] = shlex_split[10].strip()
+                        distance["dst_resid"] = shlex_split[13].strip()
+                        distance["dst_atom"] = shlex_split[16].strip().replace("^", "'")
+                    else:
+                        distance["src_segid"] = "A"
+                        distance["src_resid"] = shlex_split[2].strip()
+                        distance["src_atom"] = shlex_split[5].strip().replace("^", "'")
+                        distance["dst_segid"] = "A"
+                        distance["dst_resid"] = shlex_split[10].strip()
+                        distance["dst_atom"] = shlex_split[13].strip().replace("^", "'")
+
+                else:
+                    print 'Cannot process "%s"' % restraint_line
+                    continue
+            except IndexError:
+                print 'Cannot process "%s"' % restraint_line
+                continue
+            except ValueError:
+                print 'Cannot process "%s"' % restraint_line
+                continue
+
+            restraints["connections"].append(distance)
+
+        count += _draw_restraint(
+            restraints,
+            line_color,
+            line_width,
+            single,
+            quiet,
+            per_residue,
+            per_atom,
+            advanced_coloring
+        )
+
+    if not quiet:
+        print ' Info: Created distance objects for %d restraints' % count
+
+    cmd.order("NOE*", "yes")
+
+
+def _draw_restraint(restraints, line_color, line_width, single, quiet, per_residue, per_atom, advanced_coloring):
+    cnt = 0
+    for restraint in restraints["connections"]:
+        cnt += 1
+        sele1 = 'segid %s and resi %s and name %s' % (
+            restraint["src_segid"], restraint["src_resid"], restraint["src_atom"].replace("#", "*")
+        )
+        sele2 = 'segid %s and resi %s and name %s' % (
+            restraint["dst_segid"], restraint["dst_resid"], restraint["dst_atom"].replace("#", "*")
+        )
+
+        if line_color is None:
+            if advanced_coloring == 1:
+                if restraints["distance"] < 2 or restraints["distance"] > 6:
+                    line_color = "red"
+                elif 5 < restraints["distance"] < 6:
+                    line_color = "orange"
+                else:
+                    line_color = "splitpea"
+            else:
+                line_color = "yellow"
+
+        if per_atom or per_residue:
+            for x, y, z in [
+                [restraint["dst_segid"], restraint["dst_resid"], restraint["dst_atom"]],
+                [restraint["src_segid"], restraint["src_resid"], restraint["src_atom"]]
+            ]:
+                if per_atom:
+                    label = "NOE_%s_%s_%s" % (x, y, z)
+                elif per_residue:
+                    label = "NOE_%s_%s" % (x, y)
+                if advanced_coloring == 1:
+                    label += ".%s_%i" % (restraints["ID"], cnt)
+
+                try:
+                    cmd.distance(label, sele1, sele2, quiet=quiet,
+                                 width=line_width, gap=0, label=0)
+                    _color_restraint(label, line_color)
+                except CmdException:
+                    print 'FAILED: %s - %s' % (sele1, sele2)
+                    print restraints["ID"]
+                    return 0
+
+        else:
             if single:
                 label = "NOE"
 
-            elif aria and restraint["ID"]:
-                label = "NOE_" + restraint["ID"]
-            elif aria and parent and parent["ID"]:
-                label = "NOE_" + parent["ID"]
-
-            elif per_atom:
-                label = "NOE_%s_%s_%s" % (restraint["dst_segid"], restraint["dst_resid"], restraint["dst_atom"])
-            elif per_residue:
-                label = "NOE_%s_%s" % (restraint["dst_segid"], restraint["dst_resid"])
-
-            elif restraint["parent"][:4].upper() == "ASSI":
-                label = parent_id
             else:
-                parent_id = "NOE_%d" % count
-                label = parent_id
+                label = "NOE_" + restraints["ID"]
 
             try:
                 cmd.distance(label, sele1, sele2, quiet=quiet,
                              width=line_width, gap=0, label=0)
+
+                _color_restraint(label, line_color)
             except CmdException:
                 print 'FAILED: %s - %s' % (sele1, sele2)
-                continue
+                print restraints["ID"]
+                return 0
 
-            cmd.set("dash_color", line_color, label)
-        else:
-            print line
+    return 1
 
-    cmd.order("NOE*", "yes")
 
-    if not quiet:
-        print ' Info: Created distance objects for %d restraints' % (count)
+def _color_restraint(object_name, color="yellow"):
+    cmd.set("dash_color", color, object_name)
+
 
 cmd.extend("plot_noe", plot_noe)
