80be05313adc27eb0f2fd5151c7185f4c8273b15
Osvaldo Martin <aloctavodia@gmail.com>
2015-09-14 19:52:56 -0300

    Update and rename com.py to centroid.py
    What this script is actually computing is the centroid not the center of mass. The center of mass is the mass-weighted average of the coordinates.

--- centroid.py
+++ centroid.py
@@ -1,13 +1,13 @@
 '''
-See more here: http://www.pymolwiki.org/index.php/com
+See more here: http://www.pymolwiki.org/index.php/centroid
 
 DESCRIPTION
-        get the center of mass of selection or move selection to the origin.
+        get the centroid (geometric center) of a selection or move selection to the origin.
  
 ARGUMENTS
         selection = string: a valid PyMOL selection {default: all}
         center = 0 or 1: if center=1 center the selection {default: 0}
-        returns: center of mass: [ xCOM, yCOM, zCOM ]
+        returns: centroid: [ x, y, z ]
  
 SEE ALSO
         get_extent, get_position, http://pymolwiki.org/index.php/Center_Of_Mass
@@ -42,24 +42,24 @@ from pymol import stored
 from chempy import cpv
 
 
-def COM(selection='all', center=0, quiet=1):
+def centroid(selection='all', center=0, quiet=1):
 
     model = cmd.get_model(selection)
     nAtom = len(model.atom)
 
-    COM = cpv.get_null()
+    centroid = cpv.get_null()
 
     for a in model.atom:
-        COM = cpv.add(COM, a.coord)
-    COM = cpv.scale(COM, 1. / nAtom)
+        centroid = cpv.add(centroid, a.coord)
+    centroid = cpv.scale(centroid, 1. / nAtom)
 
     if not int(quiet):
-        print ' COM: [%8.3f,%8.3f,%8.3f]' % tuple(COM)
+        print ' centroid: [%8.3f,%8.3f,%8.3f]' % tuple(centroid)
 
-    if int(center):
-        cmd.alter_state(1, selection, "(x,y,z)=sub((x,y,z), COM)",
-                        space={'COM': COM, 'sub': cpv.sub})
+    if int(move):
+        cmd.alter_state(1, selection, "(x,y,z)=sub((x,y,z), centroid)",
+                        space={'centroid': centroid, 'sub': cpv.sub})
 
-    return COM
+    return centroid
 
-cmd.extend("COM", COM)
+cmd.extend("centroid", centroid)
