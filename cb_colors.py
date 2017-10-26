a57ddb8820207b7d46540228d85e21397a6b8b71
Jared Sampson <jared.sampson@gmail.com>
2017-10-26 10:29:13 -0400

    Add cb_colors.py.
    Import colorblindfriendly and add the cb_colors menu in one step.

---
+++ cb_colors.py
@@ -0,0 +1,18 @@
+'''cb_colors.py: Add color blind-friendly colors and a GUI menu.
+
+USAGE
+
+   import cb_colors
+
+For more information, try
+
+    import colorblindfriendly
+    help(colorblindfriendly)
+
+or visit the wiki page at:
+https://pymolwiki.org/index.php/colorblindfriendly
+
+'''
+
+import colorblindfriendly
+colorblindfriendly.add_menu()
