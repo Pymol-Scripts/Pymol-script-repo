9bd19a62026cdc7f91f2ab8262577f281e082abf
Thomas Holder <thomas.holder@rentschler.de>
2024-01-18 22:54:57 +0100

    Port dynoplot to Qt
    Tested with PyQt5 5.6 and Python 3.7.9

--- plugins/dynoplot.py
+++ plugins/dynoplot.py
@@ -7,6 +7,7 @@ See more here: http://www.pymolwiki.org/index.php/dynoplot
 #  Creation Date: 8/29/05
 #
 #  Modified 2011-11-17 by Thomas Holder
+#  Ported to PyQt 2024 by Thomas Holder
 #
 #  Notes:
 #  Draw plots that display interactive data.
@@ -14,62 +15,103 @@ See more here: http://www.pymolwiki.org/index.php/dynoplot
 ###############################################
 '''
 
-from __future__ import division
-from __future__ import generators
-from __future__ import print_function
+from typing import List, Optional, Tuple
 
-import sys
 from pymol import cmd
+from pymol.Qt import QtCore, QtGui, QtWidgets
 
-if sys.version_info[0] > 2:
-    import tkinter as Tkinter
-else:
-    import Tkinter
-
-# workaround: Set to True if nothing gets drawn on canvas, for example on linux with "pymol -x"
-with_mainloop = False
+Qt = QtCore.Qt
+QPoint = QtCore.QPoint
+QColor = QtGui.QColor
 
 
-class SimplePlot(Tkinter.Canvas):
+class SimplePlot(QtWidgets.QWidget):
 
     # Class variables
     mark_size = 4
+    xlabels = tuple(range(-180, 181, 60))
+    ylabels = tuple(range(-180, 181, 30))
 
-    def __init__(self, *args, **kwargs):
-        Tkinter.Canvas.__init__(self, *args, **kwargs)
-        self.xlabels = []   # axis labels
-        self.ylabels = []
+    def __init__(self, width: int = 320, height: int = 320):
+        super().__init__()
+        self.resize(width, height)
+
+        self.name = ""
         self.spacingx = 0   # spacing in x direction
         self.spacingy = 0
         self.xmin = 0       # min value from each axis
         self.ymin = 0
-        self.lastx = 0      # previous x,y pos of mouse
-        self.lasty = 0
         self.isdown = 0    # flag for mouse pressed
-        self.item = (0,)    # items array used for clickable events
-        self.shapes = {}    # store plot data, x,y etc..
+        self.item: Optional[dict] = None    # items array used for clickable events
+        self.shapes: List[dict] = []  # store plot data, x,y etc..
         self.idx2resn = {}  # residue name mapping
         self.symbols = 0    # 0: amino acids, 1: secondary structure
 
-    def axis(self, xmin=40, xmax=300, ymin=10, ymax=290, xint=290, yint=40, xlabels=[], ylabels=[]):
+    def closeEvent(self, event) -> None:
+        cmd.delete(self.name)
+
+    def mousePressEvent(self, event) -> None:
+        button = event.button()
+        if button == Qt.LeftButton:
+            self.down(event)
+        elif button == Qt.RightButton or button == Qt.MiddleButton:
+            self.pickWhich(event)
+
+    def mouseReleaseEvent(self, event) -> None:
+        button = event.button()
+        if button == Qt.LeftButton:
+            self.up(event)
+
+    def mouseMoveEvent(self, event) -> None:
+        self.drag(event)
+
+    def paintEvent(self, event) -> None:
+        painter = QtGui.QPainter()
+        painter.begin(self)
+        # painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        self.paintAxes(painter, event.rect())
+        self.paintColorDots(painter)
+        painter.end()
+
+    def paintAxes(self, painter, rect) -> None:
+        xmin = rect.left() + 40
+        xmax = rect.right() - 19
+        ymin = rect.top() + 10
+        ymax = rect.bottom() - 29
+        xint = (ymax - ymin) // 2 + ymin
+        yint = xmin
+
+        xlabels = self.xlabels
+        ylabels = self.ylabels
 
         # Store variables in self object
-        self.xlabels = xlabels
-        self.ylabels = ylabels
         self.spacingx = (xmax - xmin) / (len(xlabels) - 1)
         self.spacingy = (ymax - ymin) / (len(ylabels) - 1)
         self.xmin = xmin
         self.ymin = ymin
 
+        pen = painter.pen()
+
         # Create axis lines
-        self.create_line((xmin, xint, xmax, xint), fill="black", width=3)
-        self.create_line((yint, ymin, yint, ymax), fill="black", width=3)
+        pen.setWidth(3)
+        painter.setPen(pen)
+        painter.drawLine(xmin, xint, xmax, xint)
+        painter.drawLine(yint, ymin, yint, ymax)
+
+        # Text box size
+        tsize = 30
 
         # Create tick marks and labels
+        pen.setWidth(2)
+        painter.setPen(pen)
         nextspot = xmin
         for label in xlabels:
-            self.create_line((nextspot, xint + 5, nextspot, xint - 5), fill="black", width=2)
-            self.create_text(nextspot, xint - 15, text=label)
+            inextspot = round(nextspot)
+            painter.drawLine(inextspot, xint + 5, inextspot, xint - 5)
+            painter.drawText(inextspot - tsize // 2, xint - 10 - tsize,
+                             tsize, tsize, Qt.AlignHCenter | Qt.AlignBottom,
+                             str(label))
+
             if len(xlabels) == 1:
                 nextspot = xmax
             else:
@@ -77,8 +119,11 @@ class SimplePlot(Tkinter.Canvas):
 
         nextspot = ymax
         for label in ylabels:
-            self.create_line((yint + 5, nextspot, yint - 5, nextspot), fill="black", width=2)
-            self.create_text(yint - 20, nextspot, text=label)
+            inextspot = round(nextspot)
+            painter.drawLine(yint + 5, inextspot, yint - 5, inextspot)
+            painter.drawText(yint - 10 - tsize, inextspot - tsize // 2,
+                             tsize, tsize, Qt.AlignVCenter | Qt.AlignRight,
+                             str(label))
             if len(ylabels) == 1:
                 nextspot = ymin
             else:
@@ -86,10 +131,6 @@ class SimplePlot(Tkinter.Canvas):
 
     # Plot a point
     def plot(self, xp, yp, meta):
-
-        # Convert from 'label' space to 'pixel' space
-        x = self.convertToPixel("X", xp)
-        y = self.convertToPixel("Y", yp)
 
         resn, color, ss = self.idx2resn.get(meta)
 
@@ -100,28 +141,48 @@ class SimplePlot(Tkinter.Canvas):
             # symbols by secondary structure
             mark = {'H': 'Oval', 'S': 'Rect'}.get(ss, 'Tri')
 
-        if mark == 'Oval':
-            create_shape = self.create_oval
-            coords = [x - self.mark_size, y - self.mark_size,
-                      x + self.mark_size, y + self.mark_size]
-        elif mark == 'Tri':
-            create_shape = self.create_polygon
-            coords = [x, y - self.mark_size,
-                      x + self.mark_size, y + self.mark_size,
-                      x - self.mark_size, y + self.mark_size]
-        else:
-            create_shape = self.create_rectangle
-            coords = [x - self.mark_size, y - self.mark_size,
-                      x + self.mark_size, y + self.mark_size]
-
         if color >= 0x40000000:
-            color = '#%06x' % (color & 0xffffff)
+            qrgb = color | 0xFF000000
         else:
-            color = '#%02x%02x%02x' % tuple([int(0xFF * i)
-                                             for i in cmd.get_color_tuple(color)])
+            qrgb = 0xFF
+            for v in cmd.get_color_tuple(color):
+                qrgb = (qrgb << 8) | int(0xFF * v)
 
-        oval = create_shape(width=1, outline="black", fill=color, *coords)
-        self.shapes[oval] = [x, y, 0, xp, yp, meta]
+        self.shapes.append({
+            "phi": xp,
+            "psi": yp,
+            "mark": mark,
+            "color": qrgb,
+            "meta": meta,
+            "dragged": False,
+        })
+
+    def paintColorDots(self, painter) -> None:
+        pen = painter.pen()
+        pen.setWidth(1)
+        painter.setPen(pen)
+
+        size = self.mark_size
+
+        for point in self.shapes:
+            # Convert from 'label' space to 'pixel' space
+            x = round(self.convertToPixel("X", point["phi"]))
+            y = round(self.convertToPixel("Y", point["psi"]))
+
+            painter.setBrush(QColor.fromRgb(point["color"]))
+
+            mark = point["mark"]
+            if mark == 'Oval':
+                painter.drawEllipse(x - size, y - size, 2 * size, 2 * size)
+            elif mark == 'Tri':
+                polygon = QtGui.QPolygon([
+                    QPoint(x, y - size),
+                    QPoint(x + size, y + size),
+                    QPoint(x - size, y + size),
+                ])
+                painter.drawConvexPolygon(polygon)
+            else:
+                painter.drawRect(x - size, y - size, 2 * size, 2 * size)
 
     # Convert from pixel space to label space
     def convertToLabel(self, axis, value):
@@ -193,32 +254,39 @@ class SimplePlot(Tkinter.Canvas):
 
         return pixel
 
+    def findClosestShape(self, event) -> Optional[dict]:
+        closest: Tuple[float, Optional[dict]] = ((self.mark_size * 4)**2, None)
+
+        ex = event.x()
+        ey = event.y()
+
+        for point in self.shapes:
+            # Convert from 'label' space to 'pixel' space
+            x = self.convertToPixel("X", point["phi"])
+            y = self.convertToPixel("Y", point["psi"])
+            dist_sq = (x - ex)**2 + (y - ey)**2
+            if dist_sq < closest[0]:
+                closest = dist_sq, point
+
+        return closest[1]
+
     # Print out which data point you just clicked on..
     def pickWhich(self, event):
 
         # Find closest data point
-        x = event.widget.canvasx(event.x)
-        y = event.widget.canvasx(event.y)
-        spot = event.widget.find_closest(x, y)
+        point = self.findClosestShape(event)
 
         # Print the shape's meta information corresponding with the shape that was picked
-        if spot[0] in self.shapes:
-            cmd.select('sele', '(%s`%d)' % self.shapes[spot[0]][5])
-            cmd.iterate('sele', 'print " You clicked /%s/%s/%s/%s`%s/%s (DynoPlot)" %' + \
-                        ' (model, segi, chain, resn, resi, name)')
+        if point is not None:
+            cmd.select('sele', '(%s`%d)' % point["meta"])
+            cmd.iterate('sele', 'print(" You clicked /%s/%s/%s/%s`%s/%s (DynoPlot)" %'
+                        ' (model, segi, chain, resn, resi, name))')
             cmd.center('byres sele', animate=1)
 
     # Mouse Down Event
     def down(self, event):
-
-        # Store x,y position
-        self.lastx = event.x
-        self.lasty = event.y
-
         # Find the currently selected item
-        x = event.widget.canvasx(event.x)
-        y = event.widget.canvasx(event.y)
-        self.item = event.widget.find_closest(x, y)
+        self.item = self.findClosestShape(event)
 
         # Identify that the mouse is down
         self.isdown = 1
@@ -227,31 +295,29 @@ class SimplePlot(Tkinter.Canvas):
     def up(self, event):
 
         # Get label space version of x,y
-        labelx = self.convertToLabel("X", event.x)
-        labely = self.convertToLabel("Y", event.y)
+        labelx = self.convertToLabel("X", event.x())
+        labely = self.convertToLabel("Y", event.y())
 
         # Convert new position into label space..
-        if self.item[0] in self.shapes:
-            self.shapes[self.item[0]][0] = event.x
-            self.shapes[self.item[0]][1] = event.y
-            self.shapes[self.item[0]][2] = 1
-            self.shapes[self.item[0]][3] = labelx
-            self.shapes[self.item[0]][4] = labely
+        if self.item is not None:
+            self.item["phi"] = labelx
+            self.item["psi"] = labely
+            self.item["dragged"] = True
 
         # Reset Flags
-        self.item = (0,)
+        self.item = None
         self.isdown = 0
 
     # Mouse Drag(Move) Event
     def drag(self, event):
 
         # Check that mouse is down and item clicked is a valid data point
-        if self.isdown and self.item[0] in self.shapes:
-
-            self.move(self.item, event.x - self.lastx, event.y - self.lasty)
-
-            self.lastx = event.x
-            self.lasty = event.y
+        if self.isdown and self.item is not None:
+            labelx = self.convertToLabel("X", event.x())
+            labely = self.convertToLabel("Y", event.y())
+            self.item["phi"] = labelx
+            self.item["psi"] = labely
+            self.update()  # repaint
 
 
 def set_phipsi(model, index, phi, psi, state=-1):
@@ -274,63 +340,53 @@ def set_phipsi(model, index, phi, psi, state=-1):
 class DynoRamaObject:
 
     def __init__(self, selection=None, name=None, symbols='', state=-1):
-        try:
-            from pymol.plugins import get_pmgapp
-            pmgapp = get_pmgapp()
-        except ImportError:
-            pmgapp = None
-
-        if pmgapp is not None:
-            rootframe = Tkinter.Toplevel(pmgapp.root)
-            parent = rootframe
-        else:
-            rootframe = Tkinter.Tk()
-            parent = rootframe
-
-        rootframe.title(' Dynamic Angle Plotting ')
-        rootframe.protocol("WM_DELETE_WINDOW", self.close_callback)
-
-        canvas = SimplePlot(parent, width=320, height=320)
-        canvas.bind("<Button-2>", canvas.pickWhich)
-        canvas.bind("<Button-3>", canvas.pickWhich)
-        canvas.pack(side=Tkinter.LEFT, fill="both", expand=1)
-        canvas.axis(xint=150,
-                    xlabels=[-180, -120, -60, 0, 60, 120, 180],
-                    ylabels=[-180, -150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150, 180])
-
-        if symbols == 'ss':
-            canvas.symbols = 1
-
         if name is None:
             try:
                 name = cmd.get_unused_name('DynoRama')
             except AttributeError:
                 name = 'DynoRamaObject'
 
-        self.rootframe = rootframe
-        self.canvas = canvas
+        self.symbols = symbols
+        self.selection = selection
         self.name = name
         self.lock = 0
         self.state = state
+
+        self._init_canvas()
 
         if name != 'none':
             auto_zoom = cmd.get('auto_zoom')
             cmd.set('auto_zoom', 0)
             cmd.load_callback(self, name)
             cmd.set('auto_zoom', auto_zoom)
-            canvas.bind("<ButtonPress-1>", canvas.down)
-            canvas.bind("<ButtonRelease-1>", canvas.up)
-            canvas.bind("<Motion>", canvas.drag)
 
-        if selection is not None:
-            self.start(selection)
+    def _init_canvas(self):
+        canvas = SimplePlot()
+        canvas.setWindowTitle(' Dynamic Angle Plotting ')
+        canvas.destroyed.connect(self.close_callback)
 
-        if with_mainloop and pmgapp is None:
-            rootframe.mainloop()
+        if self.symbols == 'ss':
+            canvas.symbols = 1
 
-    def close_callback(self):
+        canvas.name = self.name
+        self.canvas = canvas
+
+        if self.selection is not None:
+            self.start(self.selection)
+
+        canvas.show()
+
+    def __getstate__(self):
+        state = dict(self.__dict__)
+        state.pop("canvas", None)
+        return state
+
+    def __setstate__(self, state: dict):
+        self.__dict__.update(state)
+        self._init_canvas()
+
+    def close_callback(self, obj=None) -> None:
         cmd.delete(self.name)
-        self.rootframe.destroy()
 
     def start(self, sel):
         self.lock = 1
@@ -346,14 +402,14 @@ class DynoRamaObject:
             return
 
         # Loop through each item on plot to see if updated
-        for value in self.canvas.shapes.values():
+        for value in self.canvas.shapes:
             # Look for update flag...
-            if value[2]:
+            if value["dragged"]:
                 # Set residue's phi,psi to new values
-                model, index = value[5]
-                print(" Re-setting Phi,Psi: %8.2f,%8.2f" % (value[3], value[4]))
-                set_phipsi(model, index, value[3], value[4], self.state)
-                value[2] = 0
+                model, index = value["meta"]
+                print(" Re-setting Phi,Psi: %8.2f,%8.2f" % (value["phi"], value["psi"]))
+                set_phipsi(model, index, value["phi"], value["psi"], self.state)
+                value["dragged"] = False
 
 
 def rama(sel='(all)', name=None, symbols='aa', filename=None, state=-1):
@@ -387,8 +443,7 @@ cmd.auto_arg[0]['ramachandran'] = cmd.auto_arg[0]['zoom']
 
 
 def __init_plugin__(self):
-    self.menuBar.addcascademenu('Plugin', 'PlotTools', 'Plot Tools', label='DynoPlot Tools')
-    self.menuBar.addmenuitem('PlotTools', 'command', 'Launch Rama Plot', label='Rama Plot',
-                             command=lambda: DynoRamaObject('(enabled)'))
+    from pymol.plugins import addmenuitemqt
+    addmenuitemqt('Rama Plot', lambda: DynoRamaObject('(enabled)'))
 
 # vi:expandtab:smarttab
