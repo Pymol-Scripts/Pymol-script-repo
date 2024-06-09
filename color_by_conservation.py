5608c1fc1899520af1ab1b02d5a3facabb592bb1
Pedro Lacerda <pslacerda@users.noreply.github.com>
2024-06-08 23:50:55 -0300

    Independent color option for color_by_conservation.py
    Now the option `color` and `as_putty` works independent of each other.

--- color_by_conservation.py
+++ color_by_conservation.py
@@ -1,36 +1,35 @@
-'''
-See more here: http://www.pymolwiki.org/index.php/color_by_conservation
-
-    PARAMETERS
-    aln
-        (string) the alignment object name
-    names
-        (list) a list of object names that are in the alignment;
-               if (), then PyMOL will attempt to glean the names
-               from the alignment object
-    color
-        (string) valid PyMOL spectrum name
-
-    as_putty
-        (0 or 1) if 0 display is not changed, else participating objects are shown
-                 as cartoon putty, colored by the 'color' field
-'''
-
-from __future__ import print_function
-from pymol import cmd
+from pymol import cmd, CmdException
 
 
-def color_by_conservation(aln, names=(), color="rainbow", as_putty=0, _self=cmd):
-    # PyMOL doesn't yet know about object:alignment
-    # but we need to check that this exists or we might crash
-    if _self.get_type(aln) not in ("object:", "object:alignment"):
-        print("Error: Bad or incorrectly specified alignment object.")
-        return None
+@cmd.extend
+def color_by_conservation(aln, names=(), color=None, as_putty=0):
+    '''
+    See more here: http://www.pymolwiki.org/index.php/color_by_conservation
+
+        PARAMETERS
+        aln
+            (string) the alignment object name
+        names
+            (list) a list of object names that are in the alignment;
+                if (), then PyMOL will attempt to glean the names
+                from the alignment object
+        color
+            (string) valid PyMOL spectrum name
+
+        as_putty
+            (0 or 1) if 0 display is not changed, else participating objects are shown
+                    as cartoon putty, colored by the 'color' field
+    '''
+
+    if _self.get_type(aln) != "object:alignment":
+        raise CmdException("Error: Bad or incorrectly specified alignment object.")
 
     r = cmd.get_raw_alignment(aln)
 
     if names == ():
         known_objs = []
+        for x in r:
+            known_objs.append([y[0] for y in x])
         list(map(known_objs.extend, [[y[0] for y in x] for x in r]))
         known_objs = set(known_objs)
 
@@ -41,19 +40,21 @@ def color_by_conservation(aln, names=(), color="rainbow", as_putty=0, _self=cmd)
         M = len(known_objs) + 1
 
     for obj in known_objs:
-        _self.alter(obj, "b=0.0")
+        cmd.alter(obj, "b=0.0")
 
     for af in r:
         c = float(1.0 + len(af)) / float(M)
         for y in af:
-            _self.alter("%s and index %s" % (y[0], y[1]), "b=c", space={'c': c})
+            cmd.alter("%s and index %s" % (y[0], y[1]), "b=c", space={'c': c})
 
-    if as_putty != 0:
+    if int(as_putty) != 0:
         for obj in known_objs:
-            _self.show_as("cartoon", "%s" % obj)
-            _self.cartoon("putty", "%s" % obj)
-            _self.spectrum('b', color, obj)
-            _self.sort()
-            _self.rebuild()
-    return None
-cmd.extend("color_by_conservation", color_by_conservation)
+            cmd.show_as("cartoon", "%s" % obj)
+            cmd.cartoon("putty", "%s" % obj)
+
+    if color is not None:
+        for obj in known_objs:
+            cmd.spectrum('b', color, obj)
+    
+    cmd.sort()
+    cmd.rebuild()
